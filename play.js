7b5b72eb6d3d5b7117b2a06b0b9bdf0e0f1d8660
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-22 14:28:23 -0400

    importing connect function from client

--- play.js
+++ play.js
@@ -1,32 +1,7 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133' , // IP address here,
-    port: 50542 // PORT number here,
-  });
 
-  // code that does something when the connection is first established
-  conn.on("connect", () => {
-    console.log("Isabel Connected to snek server");
-    
-  });
+const {connect} = require("./client.js");
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //code that does something when the connection is terminated
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-  conn.on('end', () => {
-    console.log('Isabel disconnected from server');
-  });
- 
-  return conn;
-};
 
 console.log("Connecting ...");
 
